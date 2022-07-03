118d119ced937746897c08751ae2cd71ade22389
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-07-04 02:17:01 +0300

    Работа

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,7 @@ const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    body: document.querySelector("[data-body]") 
 };
 
 refs.openModalBtn.addEventListener("click", toggleModal);
@@ -10,5 +11,6 @@ refs.closeModalBtn.addEventListener("click", toggleModal);
 
 function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll")
 }
 })();
